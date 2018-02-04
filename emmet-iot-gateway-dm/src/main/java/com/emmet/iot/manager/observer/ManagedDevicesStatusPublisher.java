c43f874c929bdc89f1f387d21dd74536edca8954
kuochunchang <guojun.tw@gmail.com>
2018-02-04 20:01:25 +0800

    reset

--- emmet-iot-gateway-dm/src/main/java/com/emmet/iot/manager/observer/ManagedDevicesStatusPublisher.java
+++ emmet-iot-gateway-dm/src/main/java/com/emmet/iot/manager/observer/ManagedDevicesStatusPublisher.java
@@ -33,7 +33,7 @@ public class ManagedDevicesStatusPublisher extends BaseDeviceStatusObserver {
 
 	@Bean
     TopicExchange exchange() {
-        return new TopicExchange(deviceStatusTopic, true, true );
+        return new TopicExchange(deviceStatusTopic, false, false);
     }
 	
 	@PostConstruct
